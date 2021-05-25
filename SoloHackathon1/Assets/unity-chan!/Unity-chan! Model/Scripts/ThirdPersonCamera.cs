1236894aa99efc0c762b3dd16b0970aca10a78d3
chanmin0334 <chanmin0334@naver.com>
2021-05-25 13:15:06 +0900

    Move Update
    움직이는거 만지는 중

--- SoloHackathon1/Assets/unity-chan!/Unity-chan! Model/Scripts/ThirdPersonCamera.cs
+++ SoloHackathon1/Assets/unity-chan!/Unity-chan! Model/Scripts/ThirdPersonCamera.cs
@@ -41,10 +41,7 @@ namespace UnityChan
 			if (Input.GetButton ("Fire1")) {	// left Ctlr	
 				// Change Front Camera
 				setCameraPositionFrontView ();
-			} else if (Input.GetButton ("Fire2")) {	//Alt	
-				// Change Jump Camera
-				setCameraPositionJumpView ();
-			} else {	
+			}else {	
 				// return the camera to standard position and direction
 				setCameraPositionNormalView ();
 			}
@@ -70,14 +67,6 @@ namespace UnityChan
 			bQuickSwitch = true;
 			transform.position = frontPos.position;	
 			transform.forward = frontPos.forward;
-		}
-
-		void setCameraPositionJumpView ()
-		{
-			// Change Jump Camera
-			bQuickSwitch = false;
-			transform.position = Vector3.Lerp (transform.position, jumpPos.position, Time.fixedDeltaTime * smooth);	
-			transform.forward = Vector3.Lerp (transform.forward, jumpPos.forward, Time.fixedDeltaTime * smooth);		
 		}
 	}
 }
